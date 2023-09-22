23837a2bc81d8bd2c9056096055e6cc1951a2d17
Hana <nanaelaam@gmail.com>
2023-09-21 17:31:09 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,14 @@
 #include "main.h"
-/***/
+/**
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * Return: void
+*/
 
-char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src)
 {
-	int a = -1, i;
+int a = -1, i;
 for (i = 0; dest[i] != '\0'; i++)
 ;
 
